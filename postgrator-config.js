be0dd11b083f7878b0b2f29a59667ff9ccb75369
Daniel Sottile <danielsottile@gmail.com>
2020-05-12 16:01:46 -0400

    need to fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  'migrationDirectory': 'migrations',
-  'driver': 'pg',
-  'connectionString': process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
-  'ssl': !!process.env.SSL
+  driver: 'pg',
+  connectionString: process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL
 };
