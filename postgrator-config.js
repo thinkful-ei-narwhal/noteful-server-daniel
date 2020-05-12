0b9eadb25144549f8169e2ab6821273d643274e6
Daniel Sottile <danielsottile@gmail.com>
2020-05-12 14:04:55 -0400

    updated to work with heroku, hopefully

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  driver: 'pg',
-  connectionString: process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URL : process.env.DB_URL
+  'driver': 'pg',
+  'connectionString': process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URL : process.env.DATABASE_URL,
+  'ssl': !!process.env.SSL
 };
