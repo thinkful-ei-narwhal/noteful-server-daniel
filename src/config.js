0bff4b0006806709489f850395a7345d2e2dcc16
Daniel Sottile <danielsottile@gmail.com>
2020-05-12 15:23:39 -0400

    config.js;

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'my-secret'
+  API_TOKEN: process.env.API_TOKEN || 'my-secret',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful'
 };
